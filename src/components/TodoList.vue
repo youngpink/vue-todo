60ae3cb8ac5ee11782213cab0eb45fae7af9e2f7
Ping Yang <pingyang@thoughtworks.com>
2019-11-18 15:03:48 +0800

    display tasks data.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>{{ msg }}</div>
+  <div>{{ JSON.stringify(tasks) }}</div>
 </template>
 
 <script>
@@ -7,6 +7,17 @@ export default {
   name: 'TodoList',
   props: {
     msg: String
+  },
+  data: function () {
+    return {
+      tasks: [
+        { id: '001', title: '起床', content: '早上7点起床', status: 'TODO' },
+        { id: '002', title: '吃早餐', content: '早上8点前吃完早餐', status: 'TODO' },
+        { id: '003', title: '读书', content: '读《重构》1-10页', status: 'TODO' },
+        { id: '004', title: '锻炼', content: '步行1万步', status: 'TODO' },
+        { id: '005', title: '打电话', content: '给狗娃纸打电话', status: 'TODO' },
+      ],
+    };
   }
 }
 </script>
