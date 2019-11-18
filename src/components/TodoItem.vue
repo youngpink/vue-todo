2373f998ea32a5509c00b6ebfb933d1db50d78c4
Ping Yang <pingyang@thoughtworks.com>
2019-11-18 15:14:38 +0800

    display task item data.

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ task | JSON }}</div>
+</template>
+
+<script>
+  export default {
+    name: 'TodoItem',
+    props: {
+      task: Object,
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,25 +1,30 @@
 <template>
-  <div>{{ JSON.stringify(tasks) }}</div>
+  <div>
+    <TodoItem :key="task.id" :task="task" v-for="task in tasks"></TodoItem>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'TodoList',
-  props: {
-    msg: String
-  },
-  data: function () {
-    return {
-      tasks: [
-        { id: '001', title: '起床', content: '早上7点起床', status: 'TODO' },
-        { id: '002', title: '吃早餐', content: '早上8点前吃完早餐', status: 'TODO' },
-        { id: '003', title: '读书', content: '读《重构》1-10页', status: 'TODO' },
-        { id: '004', title: '锻炼', content: '步行1万步', status: 'TODO' },
-        { id: '005', title: '打电话', content: '给狗娃纸打电话', status: 'TODO' },
-      ],
-    };
+  import TodoItem from "./TodoItem";
+
+  export default {
+    name: 'TodoList',
+    components: { TodoItem },
+    props: {
+      msg: String
+    },
+    data: function () {
+      return {
+        tasks: [
+          { id: '001', title: '起床', content: '早上7点起床', status: 'TODO' },
+          { id: '002', title: '吃早餐', content: '早上8点前吃完早餐', status: 'TODO' },
+          { id: '003', title: '读书', content: '读《重构》1-10页', status: 'TODO' },
+          { id: '004', title: '锻炼', content: '步行1万步', status: 'TODO' },
+          { id: '005', title: '打电话', content: '给狗娃纸打电话', status: 'TODO' },
+        ],
+      };
+    }
   }
-}
 </script>
 
 <style scoped>
