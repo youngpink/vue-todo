871be17a19b8c0c5383c301a9bb17300a1408e27
Ping Yang <pingyang@thoughtworks.com>
2019-11-19 15:22:30 +0800

    display count of tasks in tabs.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="home">
-    <router-link to="todo">待完成</router-link>
-    <router-link to="done">已完成</router-link>
+    <router-link to="todo">待完成 ({{ todoTasks.length }})</router-link>
+    <router-link to="done">已完成 ({{ doneTasks.length }})</router-link>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-  
+  import { mapGetters } from "vuex";
+
   export default {
     name: 'Home',
+    computed: {
+      ...mapGetters(['todoTasks', 'doneTasks'])
+    },
   }
 </script>
 
