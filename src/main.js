ff146c9e85418d9f7e6f80ece370494e63556e8f
Ping Yang <pingyang@thoughtworks.com>
2019-11-25 11:30:15 +0800

    overwrite scss variables in local scss file.

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import router from './router';
 import store from './store';
 import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import './styles/common.scss';
+import './styles/index.scss';
 
 Vue.use(ElementUI);
 
